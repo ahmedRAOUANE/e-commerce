9bedb860981f5efe1eab9ad8ea24622c252ee0cb
ahmedRAOUANE <ahmedraouane30@gmail.com>
2024-01-02 10:43:52 -0800

    #re-build the app #new file structure #using mui for ui #install react-readux & @reduxjs/toolkit #remove the old files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import React from "react";
-// css styles
-import './assets/App.css';
-
-//  components
-import Header from "./components/header/Header";
-import Mainbody from "./components/main/Mainbody";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Mainbody />
+      App
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// css styles
-import './assets/index.css';
-import './assets/normelize.css';
-import './assets/bootstrap.min.css';
-
 // components
 import App from './App';
 
