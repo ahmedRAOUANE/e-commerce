1dd8fe7d55ff4f00b1ccdd4c65d15d5c67c6034d
ahmedRAOUANE <ahmedraouane30@gmail.com>
2024-04-14 09:47:56 -0800

    complete updates

--- src/firebase.js
+++ src/firebase.js
@@ -2,11 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestoretore } from "@firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAdjCyj4STwPDVTceBN8DK1lA-VuXc-Hh4",
     authDomain: "astore-2cb37.firebaseapp.com",
